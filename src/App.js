cb85a5d943b0b9c86066c1ed5cf4eb8b97a64c38
Pavel Gatin <imnotcreative@yandex.ru>
2022-03-28 19:46:40 +0700

    Добавить возможность сортировки карточек.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import UserFilters from './components/UserFilters';
 import UsersList from './components/UsersList'
+import styles from './styles/App.scss'
 
 export default function App() {
   const [usersData, setUsersData] = React.useState(null);
@@ -10,10 +12,31 @@ export default function App() {
       .then(data => setUsersData(data));
   }, []);
 
+  function handleSortingByCity() {
+    const compareByCities = function(userA, userB) {
+      return userA.address.city.localeCompare(userB.address.city);
+    }
+
+    setUsersData(prevUsersData => prevUsersData.slice().sort(compareByCities));
+  }
+
+  function handleSortingByCompanies() {
+    const compareByCompanies = function(userA, userB) {
+      return userA.company.name.localeCompare(userB.company.name);
+    }
+
+    setUsersData(prevUsersData => prevUsersData.slice().sort(compareByCompanies));
+  }
+
   return (
     usersData
     ?
-    <UsersList usersData={usersData} />
+    <div className={styles.main}>
+      <UserFilters handleSortingByCity={handleSortingByCity}
+                   handleSortingByCompanies={handleSortingByCompanies}
+      />
+      <UsersList usersData={usersData} />
+    </div>
     :
     null
   );
---
+++ src/components/UserFilters.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/UserFilters.scss'
+
+export default function UserFilters({handleSortingByCity, handleSortingByCompanies}) {
+  return (
+    <div className={styles.filters}>
+      <span>Сортировка</span>
+      <button onClick={handleSortingByCity}>по городу</button>
+      <button onClick={handleSortingByCompanies}>по компании</button>
+    </div>
+  )
+}
